d7e3b61f3166079fd368b7c4ae56cf64cb38b428
onpux <onputrevor1@gmail.com>
2025-03-20 13:56:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const navMenu = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav a'); // Captura los enlaces del menú
 
 if (menuToggle && navMenu) {
     menuToggle.addEventListener('click', function() {
         navMenu.classList.toggle('active');
+    });
+
+    // Cierra el menú cuando se hace clic en un enlace
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            navMenu.classList.remove('active');
+        });
     });
 }
